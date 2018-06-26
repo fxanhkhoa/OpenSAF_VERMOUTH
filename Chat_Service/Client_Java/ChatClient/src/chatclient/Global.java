00e7adef6c9ed8f901b658f4ce5809911b19b46a
fxanhkhoa <fxanhkhoa@gmail.com>
2018-06-26 11:35:43 +0700

    refix merge with Khoa_Branch

--- Chat_Service/Client_Java/ChatClient/src/chatclient/Global.java
+++ Chat_Service/Client_Java/ChatClient/src/chatclient/Global.java
@@ -10,7 +10,59 @@ package chatclient;
  * @author shawry
  */
 public class Global {
-    public static final String SERVERIP = "localhost";
-    public static final int SERVERPORT = 9000;
-    public static Client cl = new Client();
+    
+    /*
+    Constant status of function return
+    */
+    public static final int OK = 0;
+    
+    /*
+    private variables
+    */
+    private static String SERVERIP = "localhost";
+    private static int SERVERPORT = 9000;
+    private static Global instance;
+    
+    /*
+    public variables
+    */
+    public static Client client;
+    
+    /*
+    private function
+    */
+    
+    
+    /*
+    public funtion
+    */
+    public Global(){
+        client = new Client();
+    }
+    
+    public int SetServerIP(String IP){
+        SERVERIP = IP;
+        return OK;
+    }
+    
+    public String GetServerIP(){
+        return SERVERIP;
+    }
+    
+    public int SetServerPORT(int Port){
+        SERVERPORT = Port;
+        return OK;
+    }
+    
+    public int GetServerPORT(){
+        return SERVERPORT;
+    }
+    
+    public static synchronized Global getInstance(){
+        if (instance == null){
+            instance = new Global();
+        }
+        return instance;
+    }
 }
+
